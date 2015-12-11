de0ad509dfa3a6c06186de24fed4a2d183ce8e15
Cellmon95 <lucas.arne@hotmail.com>
2015-12-12 00:26:08 +0100

    best :D :boom:

--- LD34/LD34/Program.cs
+++ LD34/LD34/Program.cs
@@ -10,7 +10,7 @@ namespace LD34
     {
         static void Main(string[] args)
         {
-			Console.WriteLine("Hello Mate, how are you doi!");
+			Console.WriteLine("Hello Mate, how are you doing!");
         }
     }
 }
