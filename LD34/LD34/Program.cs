07f69ed7621203f06690a2261c2e30e7983ecc48
Cellmon95 <lucas.arne@hotmail.com>
2015-12-12 00:44:41 +0100

    Hahahaha :boom:

--- LD34/LD34/Program.cs
+++ LD34/LD34/Program.cs
@@ -10,7 +10,7 @@ namespace LD34
     {
         static void Main(string[] args)
         {
-			Console.WriteLine("Hello Github!");
+			Console.WriteLine("Hello Linus! I am yure father!!!:boom:");
         }
     }
 }
