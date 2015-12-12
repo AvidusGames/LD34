64c47b8241b7c5986da5b51d682d8cd5abf89b03
Linus <linus122xbb@gmail.com>
2015-12-12 09:50:59 +0100

    Added entity

---
+++ LD34/GameCore/Entities/Entity.cs
@@ -0,0 +1,28 @@
+﻿using GameCore.Interfaces;
+using SFML.Graphics;
+using SFML.System;
+using System;
+
+namespace GameCore.Entities
+{
+    public abstract class Entity : IUpdatable, Drawable, IDisposable
+    {
+
+        public Vector2f Position { get; protected set; }
+
+        public Entity(Vector2f pos)
+        {
+            Position = pos;
+        }
+
+        public bool Destroyed { get; set; }
+
+        public abstract void Draw(RenderTarget target, RenderStates states);
+
+        public abstract void Update();
+
+        public abstract void FixedUpdate();
+
+        public abstract void Dispose();
+    }
+}
